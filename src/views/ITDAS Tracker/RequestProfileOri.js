a6fb0da8968566bc795d21ee351a78afb2e3f561
Saidatul Akmar Binti Rameley <saidatulakmar@tm.com.my>
2020-07-01 11:49:24 +0800

    changes

--- src/views/ITDAS Tracker/RequestProfileOri.js
+++ src/views/ITDAS Tracker/RequestProfileOri.js
@@ -42,12 +42,14 @@ class requestProfile extends Component {
         <Row>
           <Col xs ='12'>
           <Card>
-            <CardHeader>Request Profile</CardHeader>
+            <CardHeader>REQUEST PROFILE</CardHeader>
             <CardBody>
               <Row>
                 <Col xs='3'>
-                <Label>Reference</Label>
+                <Label>Reference #</Label>
                 <Input type="text" id="reference"name="reference"/>
+                <Label>Ext Ref # (IRIS No/Proj No)</Label>
+                <Input type="text" id="irisnoprojno"name="irisnoprojno"/>
                 <Label >Category</Label>
                 <Input type="select" name="category" id="categorybe">
                         <option value="">Please select</option>
@@ -56,42 +58,51 @@ class requestProfile extends Component {
                         <option value="MVP">MVP</option>
                 </Input>
                 <Label>Type</Label>
-                <Input type="select" name="type" id="typebe">
+                <Input type="select" name="type" id="type">
                         <option value="">Please select</option>
                         <option value="product">Product</option>
                         <option value="non-product">Non-Product</option>    
                 </Input>
-                <Label>System</Label>
-                <Input type="select" name="system" id="systemid">
-                        <option value="0">Please select</option>
-                        <option value="product">Product</option>
-                        <option value="non-product">Non-Product</option>         
-                </Input>
-                <Label >System/Ref/Project/Initiative Name</Label>
-                <Input type="text" id="projectName"name="projectName"/>
-
-                <Label >Name/Description</Label>
-                <Input type="textarea" id="descriptionid"name="description"/>
-                </Col>
-                <Col xs='3'>
                 <Label>Agile</Label>
-                <Input type="select" name="system" id="system">
+                <Input type="select" name="agile" id="agile">
                         <option value="">Please select</option>
                         <option value="yes">Yes</option>
-                        <option value="no">No</option>
-                        
+                        <option value="no">No</option>             
                 </Input>
                 <Label>AOP</Label>
                 <Input type="select" name="aop" id="aop">
                         <option value="">Please select</option>
                         <option value="yes">Yes</option>
-                        <option value="no">No</option>
-                        
+                        <option value="no">No</option>       
                 </Input>
                 <Label>Ana Plan No/Ref. No</Label>
-                <Input type="text" id="planNo"name="planNo"/>
-                <Label>Impacted Systems</Label>
-                <Input type="textarea" id="impactedsystem"name="impactedsystem"/>
+                <Input type="text" id="anaplanno"name="anaplanno"/>
+                </Col>
+
+                <Col xs='3'>
+                <Label>Plan PBE No</Label>
+                <Input type="text" id="planpbeno"name="planpbeno"/>             
+                <Label>Plan PBE Date/RFS Date</Label>
+                <Input type="date" id="planpbedate"name="planpbedate"/>
+                <Label>Actual PBE No</Label>
+                <Input type="text" id="actualpbeno"name="actualpbeno"/>
+                <Label>Actual PBE Date/RFS Date</Label>
+                <Input type="date" id="actualpbedate"name="actualpbedate"/>
+                <Label>Quarterly Plan RFS Date</Label>
+                <Input type="select" name="select" id="select">
+                        <option value="">Please select</option>
+                        <option value="Q1">Q1</option>
+                        <option value="Q2">Q2</option>
+                        <option value="Q3">Q3</option>
+                        <option value="Q4">Q4</option>                     
+                </Input>
+                </Col>
+
+                <Col xs='3'>
+                <Label >Name/Description</Label>
+                <Input type="textarea" id="description"name="description"/>
+                <Label >Remarks/Benefit</Label>
+                <Input type="textarea" id="latestremark"name="latestremark"/>
                 <Label>System</Label>
                 <Input type="select" name="systemcategory" id="systemcategory">
                         <option value="">Please select</option>
@@ -103,24 +114,20 @@ class requestProfile extends Component {
                         <option value="KIV">KIV</option>
                         <option value="Cancel">Cancel</option>
                         <option value="Close">Close</option>
-                        
                 </Input>
                 </Col>
                 <Col xs='3'>
-                <Label>Plan PBE No</Label>
-                <Input type="text" id="planpbeno"name="planpbeno"/>
-                <Label>Plan PBE Date/RFS Date</Label>
-                <Input type="date" id="planpbedate"name="planpbedate"/>
-                <Label>Quarterly Plan RFS Date</Label>
+                <Label>System/Ref/Project/Initiative Name</Label>
+                <Input type="text" id="initiativename"name="initiativename"/>
+                <Label>Status</Label>
                 <Input type="select" name="select" id="select">
-                        <option value="">Please select</option>
-                        <option value="Q1">Q1</option>
-                        <option value="Q2">Q2</option>
-                        <option value="Q3">Q3</option>
-                        <option value="Q4">Q4</option>
+                        <option value="0">Please select</option>
+                        <option value="SIT">SIT</option>
+                        <option value="UAT">UAT</option>
+                        <option value="Regression">Regression</option>
+                        <option value="PT">PT</option>
+                        <option value="Pre Prod">Pre Prod</option>
                 </Input>
-                <Label >Latest Remark/Update</Label>
-                <Input type="textarea" id="latestremark"name="latestremark"/>
                 <Label>Status Description</Label>
                 <Input type="select" name="select" id="select">
                         <option value="0">Please select</option>
@@ -130,12 +137,6 @@ class requestProfile extends Component {
                         <option value="PT">PT</option>
                         <option value="Pre Prod">Pre Prod</option>
                 </Input>
-                </Col>
-                <Col xs='3'>
-                <Label>Actual PBE No</Label>
-                <Input type="text" id="actualpbeno"name="actualpbeno"/>
-                <Label>Actual PBE Date</Label>
-                <Input type="date" id="actualpbedate"name="actualpbedate"/>
                 </Col> 
               </Row>
             </CardBody>
