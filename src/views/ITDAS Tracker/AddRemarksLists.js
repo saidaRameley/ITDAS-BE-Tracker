81294217c252d2cb1b63f84dda70263c857be005
Saidatul Akmar Binti Rameley <saidatulakmar@tm.com.my>
2020-07-07 17:14:54 +0800

    changes

--- src/views/ITDAS Tracker/AddRemarksLists.js
+++ src/views/ITDAS Tracker/AddRemarksLists.js
@@ -81,6 +81,7 @@ class AddRemarksList extends Component {
                 </CardBody>
               </Row>            
             </CardBody>
+  
       <Col xs='12'>
     <table class="table table-bordered table-striped table table-sm">
   <thead>
